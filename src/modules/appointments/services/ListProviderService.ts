f11ba17877289953a5a67f0b31754ce776c8bdba
Daniel Ataydes <daniel_ataydes@msn.com>
2020-10-08 10:23:40 -0300

    Criado a cache da lista de providers

--- src/modules/appointments/services/ListProviderService.ts
+++ src/modules/appointments/services/ListProviderService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from "tsyringe"
-
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/Users';
@@ -13,14 +13,23 @@ class ListProvidersService {
     constructor(
         @inject('UsersRepository')
         private userReposiroty: IUsersRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) { }
 
     public async execute({ user_id }: IRequest): Promise<User[]> {
-        const users = await this.userReposiroty.findAllProviders({
-            except_user_id: user_id,
-        });
+        let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
 
+        if (!users) {
+            users = await this.userReposiroty.findAllProviders({
+                except_user_id: user_id,
+            });
+            console.log('A query no banco foi feita.');
+            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+        }
         return users;
+
     }
 }
 export default ListProvidersService;
