08387b2a839909fde132de78ffcc2316b0809302
Daniel Ataydes <daniel_ataydes@msn.com>
2020-10-26 10:11:44 -0300

    Correções de rota

--- src/modules/appointments/services/ListProviderService.ts
+++ src/modules/appointments/services/ListProviderService.ts
@@ -3,6 +3,7 @@ import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICa
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '@modules/users/infra/typeorm/entities/Users';
+import { classToClass } from "class-transformer";
 
 interface IRequest {
     user_id: string;
@@ -20,16 +21,20 @@ class ListProvidersService {
 
     public async execute({ user_id }: IRequest): Promise<User[]> {
         let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
-
+        // let users; // pulando o cache.
         if (!users) {
             users = await this.userReposiroty.findAllProviders({
                 except_user_id: user_id,
             });
             console.log('A query no banco foi feita.');
-            await this.cacheProvider.save(`providers-list:${user_id}`, users);
+            await this.cacheProvider.save(
+                `providers-list:${user_id}`,
+                classToClass(users),
+            );
         }
-        return users;
 
+        return users;
     }
 }
+
 export default ListProvidersService;
